44aea5680c2a4ccff40c8e517aa675c1923c87bd
Mariyam Alim <mariyam.muhammadalim@ontariotechu.net>
2023-03-16 21:51:58 -0400

    Used move_ip for moving rectangle, rectangle now keeps moving in 1 direction like snake is supposed to

--- snake_game.py
+++ snake_game.py
@@ -23,35 +23,64 @@ def load_image(name):
     return image
 
 class Snake:
-    def __init__(self, size, x, y, v = 0.3):
+    def __init__(self, size, x, y, v = 0.05):
         self.x = x
         self.y = y
         self.width = 25
         self.height = 25
         self.v = v
-        # self.rect = pygame.rect.Rect([0, 0, tile_size - 2, tile_size - 2])
-        # self.rect.center = [2, 3] #generate random number
-        self.direction = "right"
-        # self.time = 0
-        # self.length = 1
-        #self.segments = [self.rect.copy(), self.rect.copy()]
+    
+        self.rect = pygame.rect.Rect([0, 0, tile_size - 2, tile_size - 2])
+        self.rect.center = [self.x, self.y]  # set the position of the head
+        self.direction = [0, 0]
+        self.time = 0
+        self.length = 1
+        self.segments = [self.rect.copy()]  # add the head to the segments list
+
+        # initialize the positions of the remaining segments based on the head's position
+        for i in range(self.length - 1):
+            segment_rect = pygame.rect.Rect([0, 0, tile_size - 2, tile_size - 2])
+            segment_rect.center = [self.x - (i + 1) * tile_size, self.y]
+            self.segments.append(segment_rect)
 
     def draw(self):
-        #[pygame.draw.rect(screen, 'green', segment) for segment in self.segments]
-        pygame.draw.rect(screen, GREEN, (self.x, self.y, self.width, self.height))
+        [pygame.draw.rect(screen, 'green', segment) for segment in self.segments]
+        # pygame.draw.self.rect(screen, GREEN, (self.x, self.y, self.width, self.height))
+    
+    def control_snake(self, event):
+        if event.type == pygame.QUIT:
+            done = True
+
+        elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_a:
+                    self.direction = [-1, 0]
+                elif event.key == pygame.K_d:
+                    self.direction = [1, 0]
+                elif event.key == pygame.K_w:
+                    self.direction = [0, -1]
+                elif event.key == pygame.K_s:
+                    self.direction = [0, 1]
+                elif event.key == pygame.K_q:
+                    pygame.quit()
+
     
     def move_snake(self):
-        if self.direction == "right":
-            self.x += self.v
-        elif self.direction == "left":
-            self.x -= self.v
-        elif self.direction == "up":
-            self.y -= self.v
-        elif self.direction == "down":
-            self.y += self.v
-    
-    def turn_snake(self, direction):
-        self.direction = direction
+        self.rect.move_ip(self.direction[0], self.direction[1])
+
+        for i in range(len(self.segments)):
+            index = len(self.segments) - i - 1
+            if index == 0:
+                self.segments[index].center = self.rect.center
+            else:
+                self.segments[index].center = self.segments[index-1].center
+
+        self.segments.append(self.rect.copy())
+        self.segments = self.segments[-self.length:]
+
+    def eat_prey(self):
+        self.segments.append(self.rect.copy())
+        self.segments = self.segments[-self.length:]
+
 
 
 clock = pygame.time.Clock()
@@ -71,31 +100,15 @@ done = False
 
 while not done:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            done = True
+        snake.control_snake(event)
 
-
-        elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_a:
-                    snake.turn_snake("left")
-                    snake.move_snake
-                elif event.key == pygame.K_d:
-                    snake.turn_snake("right")
-                    snake.move_snake
-                elif event.key == pygame.K_w:
-                    snake.turn_snake("up")
-                    snake.move_snake
-                elif event.key == pygame.K_s:
-                    snake.turn_snake("down")
-                    snake.move_snake
-                elif event.key == pygame.K_q:
-                    pygame.quit()
     snake.move_snake()
 
-   # draw snake
     screen.fill(WHITE)
     snake.draw()
     pygame.display.update()
+
+    clock.tick(60)
 #     status = True
 # while (status):
 
