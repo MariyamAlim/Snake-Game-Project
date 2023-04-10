dfb0531955f2581043b978f59da210c93cb14bb5
Mariyam Alim <90260594+MariyamAlim@users.noreply.github.com>
2023-04-10 14:07:08 -0400

    Update snake_game3.py

--- snake_game3.py
+++ snake_game3.py
@@ -2,17 +2,30 @@ import numpy as np
 import pygame
 from prey import *
 
-WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 
+"""
+Snake class provides the user controlled snake object
+"""
+
 class Snake:
+
+    """
+    init initializes the following attributes of the snake:
+    - the initial position of the head and body of the snake
+    - the two prey objects
+    - the velocity speed of the snake as well as the velocity in the x and y directions
+    - size of the snake blocks
+    - the direction the snake is moving in as well as the list that will store the directions the snake CAN move in
+    - the score
+    """
     def __init__(self, x, y, prey, prey2, tile_size):
         self.x = x
         self.y = y
 
         self.prey = prey
         self.prey2 = prey2
-        # self.tile_size = self.prey.v *tile_size
+
         self.v = 0.6
         self.tile_size = tile_size
         self.vx = self.tile_size * self.v
@@ -28,6 +41,14 @@ class Snake:
 
         self.score = 0
 
+
+    """
+    draw method draws the following:
+    - the snake
+    - the 1st prey object
+    - the 2nd pray object once the score reaches 15
+    - the score
+    """
     def draw(self, screen, color):
         for x, y in self.body:
             rect = pygame.rect.Rect([0, 0, self.tile_size, self.tile_size])
@@ -36,37 +57,38 @@ class Snake:
         pygame.draw.rect(screen, color, self.rect)
         self.prey.draw(screen)
 
-        if self.score > 10:
+        if self.score > 15:
             self.prey2.draw(screen)
 
         font = pygame.font.Font('freesansbold.ttf', 20)
         text = font.render("Score: " + str(self.score), True, BLACK, None)
         screen.blit(text, [10, 10])
 
-
-# fixed the display of 2 rect issue
-# vx was set to the tile size (width of each square)
-# body is initialized with 2 tuples which are (x, y) and (x - self.tile_size, y) to
-# to position the squares horizontally
-
+    """
+    control_snake updates the following values based on keyboard input (WASD keys):
+    - the direction the snake is moving in
+    - the list of directions the snake CAN move in (Note: the snake can't move backwards)
+    - the snake's velocity in the x and y directions
+    if the user wants to quit the game, they can press 'Q'
+    """
     def control_snake(self, event):
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and self.directions[pygame.K_a] and self.vx == 0:
+            if event.key == pygame.K_a and self.directions[pygame.K_a] and self.vx == 0:
                 self.direction = [-1, 0]
                 self.directions = {pygame.K_w: 1, pygame.K_s: 1, pygame.K_a: 1, pygame.K_d: 0}
                 self.vx = -self.tile_size * self.v
                 self.vy = 0
-            elif event.key == pygame.K_RIGHT and self.directions[pygame.K_d] and self.vx == 0:
+            elif event.key == pygame.K_d and self.directions[pygame.K_d] and self.vx == 0:
                 self.direction = [1, 0]
                 self.directions = {pygame.K_w: 1, pygame.K_s: 1, pygame.K_a: 0, pygame.K_d: 1}
                 self.vx = self.tile_size * self.v
                 self.vy = 0
-            elif event.key == pygame.K_UP and self.directions[pygame.K_w] and self.vy == 0:
+            elif event.key == pygame.K_w and self.directions[pygame.K_w] and self.vy == 0:
                 self.direction = [0, -1]
                 self.directions = {pygame.K_w: 1, pygame.K_s: 0, pygame.K_a: 1, pygame.K_d: 1}
                 self.vx = 0
                 self.vy = -self.tile_size * self.v
-            elif event.key == pygame.K_DOWN and self.directions[pygame.K_s] and self.vy == 0:
+            elif event.key == pygame.K_s and self.directions[pygame.K_s] and self.vy == 0:
                 self.direction = [0, 1]
                 self.directions = {pygame.K_w: 0, pygame.K_s: 1, pygame.K_a: 1, pygame.K_d: 1}
                 self.vx = 0
@@ -75,6 +97,11 @@ class Snake:
                     pygame.quit()
                     quit()
 
+    """
+    move_snake moves the snake in the specified direction by updating the following:
+    - position of the head
+    - the body of the snake
+    """
     def move_snake(self):
         self.x += self.vx
         self.y += self.vy
@@ -82,19 +109,37 @@ class Snake:
         self.rect.center = [self.x, self.y]
         self.body.pop()
 
-
+    """
+    check_boundary checks whether the snake's head has hit a boundary
+    - if it has, it returns true
+    - else, false
+    """
     def check_boundary(self, win_width, win_height):
         if self.rect.right > win_width or self.rect.left < 0 or self.rect.top < 0 or self.rect.bottom > win_height:
             return True
         
         return False
 
+    """
+    self_eating checks whether the snake has collided with itself
+    - if it has, it returns true
+    - else, false
+    """
     def self_eating(self):
         for x, y in self.body[1:]:
             if self.x == x and self.y == y:
                 return True
         return False
     
+    """
+    eat_prey checks whether the snake has eaten the two preys
+    - if it has eaten the 1st prey
+        - the score increases by 1
+        - length of the snake increases by 1
+    - if it has eaten the 2nd prey
+        - the score does not increase
+        - length of the snake still increases as well as velocity
+    """
     def eat_prey(self, win_width, win_height):
         if (self.prey.rect.x - self.rect.x)**2 <= (self.tile_size/2)**2 and (self.prey.rect.y - self.rect.y)**2 <= (self.tile_size/2)**2:
             self.prey.rect.center = self.prey.get_random_pos(win_width, win_height)
@@ -112,6 +157,11 @@ class Snake:
             self.length +=1
 
 
+    """
+    check_event checks whether the snake has:
+    - collided with the window boundaries
+    - collided with itself
+    """
     def check_event(self, win_width, win_height):
         if self.check_boundary(win_width, win_height) == True or self.self_eating() == True:
             return True
